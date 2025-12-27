e7a0696bfae19738ac10033c3c939b5a1c2d4f9d
Hayaru SHOUNO <shouno@Uec.ac.jp>
2025-12-27 09:10:36 +0900

    main.py を リネーム

--- train.sh
+++ train.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+TRAIN_PY=scripts/train.py
+DATASET=cifar10
+DATAFOLDER=/workspace/data
+
 # SimCLR/BYOL/SimSiam
-python3 main.py --method simclr  --dataset cifar10 --data_dir /workspace/data/cifar10
-#python3 main.py --method byol    --dataset cifar10 --data_dir /workspace/data/cifar10
-#python3 main.py --method simsiam --dataset cifar10 --data_dir /workspace/data/cifar10
-#python3 main.py --method barlow  --dataset cifar10 --data_dir /workspace/data/cifar10
+python3 $(TRAIN_PY) --method simclr  --dataset $(DATASET) --data_dir $(DATAFOLDER)/$(DATASET) --max_epochs 10
+#python3 main.py --method byol    --dataset $(DATASET) --data_dir $(DATAFOLDER)/$(DATASET)
+#python3 main.py --method simsiam --dataset $(DATASET) --data_dir $(DATAFOLDER)/$(DATASET)
+#python3 main.py --method barlow  --dataset $(DATASET) --data_dir $(DATAFOLDER)/$(DATASET)
 
 # SwAV (メモリ使用量が多い)
-#python3 main.py --method swav    --dataset cifar10 --data_dir /workspace/data/cifar10
+#python3 main.py --method swav    --dataset $(DATASET) --data_dir $(DATAFOLDER)/$(DATASET)
 
 # MAE (メモリ使用量が多い)
-#python3 main.py --method mae     --dataset cifar10 --data_dir /workspace/data/cifar10 --batch_size 256 --max_epochs 200
+#python3 main.py --method mae     --dataset $(DATASET) --data_dir $(DATAFOLDER)/$(DATASET) --batch_size 256 --max_epochs 200
