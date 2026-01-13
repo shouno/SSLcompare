07b4bfcb771f43aaef18c5481eb6f06dad55a53c
Hayaru Shouno @ Arceus <shouno@uec.ac.jp>
2026-01-13 10:08:09 +0000

    部分部分でやってく

--- train.sh
+++ train.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 TRAIN_PY=scripts/train.py
-DATASET=cifar10
+DATASET=stl10
 DATAFOLDER=/workspace/data
 MAXEPOCHS=200
 
 # SimCLR/BYOL/SimSiam
-python3 $TRAIN_PY --method simclr       --dataset $DATASET --data_dir $DATAFOLDER/$DATASET --max_epochs $MAXEPOCHS
+#python3 $TRAIN_PY --method simclr       --dataset $DATASET --data_dir $DATAFOLDER/$DATASET --max_epochs $MAXEPOCHS
 python3 $TRAIN_PY --method byol         --dataset $DATASET --data_dir $DATAFOLDER/$DATASET --max_epochs $MAXEPOCHS
 python3 $TRAIN_PY --method simsiam      --dataset $DATASET --data_dir $DATAFOLDER/$DATASET --max_epochs $MAXEPOCHS
-python3 $TRAIN_PY --method barlowtwins  --dataset $DATASET --data_dir $DATAFOLDER/$DATASET --max_epochs $MAXEPOCHS
+#python3 $TRAIN_PY --method barlowtwins  --dataset $DATASET --data_dir $DATAFOLDER/$DATASET --max_epochs $MAXEPOCHS
 
 # SwAV (メモリ使用量が多い)
-python3 $TRAIN_PY --method swav         --dataset $DATASET --data_dir $DATAFOLDER/$DATASET --max_epochs $MAXEPOCHS
+#python3 $TRAIN_PY --method swav         --dataset $DATASET --data_dir $DATAFOLDER/$DATASET --max_epochs $MAXEPOCHS
 
 # MAE (メモリ使用量が多い), エンコーダを ViT-MAE に固定, lr を低めに設定
-python3 $TRAIN_PY --method mae          --dataset $DATASET --data_dir $DATAFOLDER/$DATASET --lr 1e-4 --base_encoder vit_mae --max_epochs $MAXEPOCHS --batch_size 256 
+#python3 $TRAIN_PY --method mae          --dataset $DATASET --data_dir $DATAFOLDER/$DATASET --lr 1e-4 --base_encoder vit_mae --max_epochs $MAXEPOCHS --batch_size 256 
